2bd6fbf8d620acf3bd7a5b6e0a41f7c4dd4cfe61
nrurnru <nrurnru@gmail.com>
2020-10-28 18:26:56 +0900

    [IOS] feat: 마일스톤 cell xib 구현
    - 레이아웃, 아웃렛 추가

--- ios/IssueTracker/IssueTracker/Views/Milestone/MilestoneCell.swift
+++ ios/IssueTracker/IssueTracker/Views/Milestone/MilestoneCell.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class MilestoneCell: UICollectionViewCell {
-
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var dueDate: UILabel!
+    @IBOutlet weak var description: UILabel!
+    @IBOutlet weak var completePercentage: UILabel!
+    @IBOutlet weak var openIssue: UILabel!
+    @IBOutlet weak var closedIssue: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
 }
