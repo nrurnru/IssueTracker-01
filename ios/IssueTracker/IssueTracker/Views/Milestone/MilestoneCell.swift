e592eb0cf73bcab013a800a3a2683783d2e46419
SeokHoKim <seokho0330@gmail.com>
2020-10-28 17:00:37 +0900

    [IOS] feat: MilestoneCell.swift 추가
    - 마일스톤 탭을 위한 컬렉션 셀 추가

---
+++ ios/IssueTracker/IssueTracker/Views/Milestone/MilestoneCell.swift
@@ -0,0 +1,17 @@
+//
+//  MilestoneCell.swift
+//  IssueTracker
+//
+//  Created by 김석호 on 2020/10/28.
+//
+
+import UIKit
+
+class MilestoneCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
