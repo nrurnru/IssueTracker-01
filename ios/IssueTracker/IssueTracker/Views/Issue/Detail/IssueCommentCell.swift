b2c3dbae642dafd5154607315612d50d114373b7
SeokHoKim <seokho0330@gmail.com>
2020-11-05 17:39:29 +0900

    [IOS] feat: IssueCommentCell 추가
    - 댓글 셀 추가

---
+++ ios/IssueTracker/IssueTracker/Views/Issue/Detail/IssueCommentCell.swift
@@ -0,0 +1,14 @@
+//
+//  IssueCommentCell.swift
+//  IssueTracker
+//
+//  Created by 김석호 on 2020/11/05.
+//
+
+import UIKit
+
+class IssueCommentCell: UICollectionViewListCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
